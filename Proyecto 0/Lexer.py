cd3e464c741878fd2603d4b0416cdc9a02ef851a
JohanBautistaQ <j.bautistaq@uniandes.edu.co>
2023-02-09 12:20:59 -0500

    Leer archivo

--- Proyecto 0/Lexer.py
+++ Proyecto 0/Lexer.py
@@ -1,1 +1,10 @@
 "No vamos a usar librerias, gracias:)"
+
+def leer_archivo(nombre_archivo):
+    with open(nombre_archivo, 'r') as archivo:
+        contenido = archivo.read()
+        palabras = contenido.split()
+        return palabras
+
+        
+
