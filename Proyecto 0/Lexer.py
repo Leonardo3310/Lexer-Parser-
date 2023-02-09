09c44e99da1461ed101995b4536b2da93ad5d284
JohanBautistaQ <j.bautistaq@uniandes.edu.co>
2023-02-09 12:35:32 -0500

    lexer

--- Proyecto 0/Lexer.py
+++ Proyecto 0/Lexer.py
@@ -1,10 +1,31 @@
-"No vamos a usar librerias, gracias:)"
+"""No vamos a usar librerias, gracias:"""
+
+#Documentacion
+"""Comentarios por desarrollar"""
 
 def leer_archivo(nombre_archivo):
     with open(nombre_archivo, 'r') as archivo:
         contenido = archivo.read()
         palabras = contenido.split()
         return palabras
+
+#devuelve una lista de las palabras contenidas en el archivo.
+
+def lexer(palabras, diccionario):
+    tokens = []
+    for palabra in palabras:
+        if palabra in diccionario:
+            tokens.append((palabra, diccionario[palabra]))
+        else:
+            tokens.append((palabra, 'UNKNOWN'))
+    return tokens
+
+#Una lista de palabras y un diccionario que asigna tokens a las palabras. 
+#La función itera sobre la lista de palabras y comprueba si cada palabra está en el diccionario. 
+#Si la palabra está, agrega una tupla a la lista tokens que incluye la palabra y su token correspondiente. 
+#Si la palabra no está, la función agrega una tupla que incluye la palabra y el token "UNKNOWN". 
+#Finalmente, la función devuelve la lista de tokens.
+
 
         
 
