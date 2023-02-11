81166702248fffee9ab75e3c06bed055fc129f55
JohanBautistaQ <j.bautistaq@uniandes.edu.co>
2023-02-10 21:56:16 -0500

    parser v2

--- Proyecto_0/Parser.py
+++ Proyecto_0/Parser.py
@@ -40,22 +40,10 @@ def parse(tokens):
         if AuxParserBetweenKeys(tokensEntreCOPENyCCLOSE) != True:
             return False
         index+=1
-
-        if tokens[index] in ["CAT", "CGT", "CM", "CT",
-    "CF", "CPUT", "CPICK", "CMTT", "CMDIR", "CJTT", "CJDIR", "IF", "ELSE","ELSEIF",    
-    "THEN", "WHL"] and \
-    index < len(tokens):
-            index += 1
-            print("q")
-
-        if tokens[index] == "NAME":
-            pass
-  
-        index += 1
-        print("a")
-        """if index < len(tokens) and tokens[index] != "DOSPUNTOS":
-                return False"""
- 
+        if AuxAfter(tokens[index:]) != False:
+            return False
+        else:
+            return True
 
     return True
 
@@ -101,6 +89,32 @@ def AuxParserBetweenKeys(tokens):
         print("a")
 
     return True
+
+ 
+def AuxAfter(tokens):
+    
+    i = 0
+    while i < len(tokens):
+        if tokens[i] in ["CAT", "CGT", "CM", "CT",
+    "CF", "CPUT", "CPICK", "CMTT", "CMDIR", "CJTT", "CJDIR", "IF", "ELSE","ELSEIF",    
+   "THEN", "WHL"]:
+            i += 1
+            if i >= len(tokens) or tokens[i] != "DOSPUNTOS":
+                return False
+        elif tokens[i] == "CCLOSE":
+            return i == len(tokens) - 1
+        elif tokens[i] == "NAME":
+            i += 1
+            if i >= len(tokens) or tokens[i] != "COPEN":
+                return False
+        elif tokens[i] in ["CNPUT", "CNPCK", "CMIDIR", "CJIDIR", "CMTT"]:
+            i += 1
+            if i >= len(tokens) or tokens[i] != "DOSPUNTOS":
+                return False
+        else:
+            return False
+        i += 1
+    return False
 
    
 
